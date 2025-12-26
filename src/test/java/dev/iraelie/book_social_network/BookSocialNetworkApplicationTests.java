743a9d5f3332650715b2b0a6a521cea7709ca2b0
Elie <niyubwayoiraelie5777@gmail.com>
2025-12-26 22:15:30 +0200

    initialized book-social-network project

---
+++ src/test/java/dev/iraelie/book_social_network/BookSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.iraelie.book_social_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
