c00fe09038cc2e0b4bf5e8cf0c1a65cd61db410c
Elie <niyubwayoiraelie5777@gmail.com>
2025-12-28 22:41:57 +0200

    Created Role entity.

---
+++ src/main/java/dev/iraelie/book_social_network/role/Role.java
@@ -0,0 +1,32 @@
+package dev.iraelie.book_social_network.role;
+
+import dev.iraelie.book_social_network.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
