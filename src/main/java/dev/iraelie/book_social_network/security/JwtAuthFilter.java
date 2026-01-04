72618bf7bcd305fc91a8538c85c3395ca246bd9a
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-04 06:32:34 +0200

    Implemented JwtAuthFilter to not apply authentication filter if there is authorization header.

--- src/main/java/dev/iraelie/book_social_network/security/JwtAuthFilter.java
+++ src/main/java/dev/iraelie/book_social_network/security/JwtAuthFilter.java
@@ -4,10 +4,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Service
 public class JwtAuthFilter extends OncePerRequestFilter {
@@ -18,6 +21,14 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
         if (request.getServletPath().contains("/api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String authHeader = request.getHeader(AUTHORIZATION);
+        final String jwt;
+        final String userEmail;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
