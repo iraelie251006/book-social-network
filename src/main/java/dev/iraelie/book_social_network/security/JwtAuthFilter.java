b25c50d36469925afe9655f1393eeff84d7ede6e
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-04 06:19:20 +0200

    Implemented JwtAuthFilter to extend OncePerRequestFilter class.

--- src/main/java/dev/iraelie/book_social_network/security/JwtAuthFilter.java
+++ src/main/java/dev/iraelie/book_social_network/security/JwtAuthFilter.java
@@ -1,7 +1,8 @@
 package dev.iraelie.book_social_network.security;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Service
-public class JwtAuthFilter {
+public class JwtAuthFilter extends OncePerRequestFilter {
 }
