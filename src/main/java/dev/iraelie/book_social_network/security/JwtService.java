b1117806aea9578a338d9bec8a661bdf7fdc4219
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-06 22:24:42 +0200

    create generateToken method.

--- src/main/java/dev/iraelie/book_social_network/security/JwtService.java
+++ src/main/java/dev/iraelie/book_social_network/security/JwtService.java
@@ -1,10 +1,16 @@
 package dev.iraelie.book_social_network.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
 public class JwtService {
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
 
 }
