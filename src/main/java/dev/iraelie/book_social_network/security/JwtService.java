65edb55f9bc14b74dafde611ac0ad99cf47d8907
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-06 22:29:39 +0200

    created generateToken method for second time.

--- src/main/java/dev/iraelie/book_social_network/security/JwtService.java
+++ src/main/java/dev/iraelie/book_social_network/security/JwtService.java
@@ -5,12 +5,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class JwtService {
+    private long jwtExpiration;
+
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> claims, UserDetails userDetails) {
+        return buildToken(claims, userDetails, jwtExpiration);
     }
 
 }
